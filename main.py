af025ffb9270687c8d9dbb4dee7b29cb541054b4
sharvari898 <deshpandesharvari889@gmail.com>
2026-01-17 14:42:51 +0530

    added doc string

--- main.py
+++ main.py
@@ -6,13 +6,25 @@ app = Flask(__name__)
 
 stations = pd.read_csv("data_small/stations.txt", skiprows = 17)
 station = stations[["STAID","STANAME                                 "]]
+
+
 @app.route("/")
 def home():
+    """
+        return home.html page
+    :return:
+    """
+
     return render_template("home.html",data = station.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
-def about(station,date):
+def station_data(station,date):
+    """
+        return data for specific station for specific year
+        :param station:
+        :return:
+        """
     df = pd.read_csv("data_small/TG_STAID" + str(station).zfill(6)+ ".txt", skiprows = 20 ,
                      parse_dates = ["    DATE"])
     temperature = df.loc[df["    DATE"] == date]["   TG"].min().squeeze() / 10
@@ -21,17 +33,32 @@ def about(station,date):
         "date" : date,
         "temperature" : temperature
     }
+
+
 @app.route("/api/v1/<station>")
 def all_station(station):
+    """
+    return data for specific station for all years
+    :param station:
+    :return:
+    """
     filename = pd.read_csv("data_small/TG_STAID" + str(station).zfill(6) + ".txt", skiprows=20,
                      parse_dates=["    DATE"])
     return filename.to_dict(orient = "records")
+
+
 @app.route("/api/v1/yearly/<station1>/<year>")
 def yearly(station1,year):
+    """
+    to return all data for specific station for mentioned year
+    :param station1:
+    :param year:
+    :return: dict
+    """
     filename = pd.read_csv("data_small/TG_STAID" + str(station1).zfill(6) + ".txt", skiprows=20)
     filename["    DATE"] = filename["    DATE"].astype(str)
     result = filename[filename["    DATE"].str.startswith(str(year))].to_dict(orient = "records")
-    print(result)
     return result
+
 if __name__ == "__main__":
     app.run(debug=True)
